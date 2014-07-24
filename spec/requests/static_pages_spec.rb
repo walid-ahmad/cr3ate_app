add22f4955a7ccac0106b271088aa8ba5eb12b7e
Walid Ahmad <walid33@gmail.com>
2014-07-23 21:50:15 -0400

    Edited static pages to have appropriate titles

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,45 @@
+require 'spec_helper'
+
+describe "Static pages" do
+
+  	describe "Home page" do
+
+	    it "should have the content 'cr3ate'" do
+	      visit '/static_pages/home'
+	      expect(page).to have_content('cr3ate')
+	    end
+
+	    it "should have the right title" do
+	    	visit '/static_pages/home'
+	    	expect(page).to have_title("cr3ate | Home")
+	    end
+	end
+
+    describe "Help page" do
+
+	    it "should have the content 'Help'" do
+	      visit '/static_pages/help'
+	      expect(page).to have_content('Help')
+	    end
+
+	    it "should have the right title" do
+	    	visit '/static_pages/help'
+	    	expect(page).to have_title("cr3ate | Help")
+	    end
+	end
+
+
+    describe "About page" do
+
+	    it "should have the content 'About Us'" do
+	      visit '/static_pages/about'
+	      expect(page).to have_content('About Us')
+	    end
+
+	    it "should have the right title" do
+	  		visit '/static_pages/about'
+	  		expect(page).to have_title("cr3ate | About")
+		end
+  	end
+
+end
