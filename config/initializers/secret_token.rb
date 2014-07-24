e6a7e2e2b511afe9f89b17598c7196df8c102a5e
Walid Ahmad <walid33@gmail.com>
2014-07-23 20:08:40 -0400

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Cr3ateApp::Application.config.secret_key_base = '0ba75ce34be1c4b1b905c865853c2658c6b58c1e16e6c1d7f6b5bdf0c0de688e0901012e7822307dea1d477d4c4a7721b69b17775c07bef6dfa413b31392308d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Cr3ateApp::Application.config.session_store :cookie_store, key: '_cr3ate_app_session'
