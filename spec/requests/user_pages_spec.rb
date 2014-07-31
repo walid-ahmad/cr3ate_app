84e64e44259a28e66f6505d4f7703f2e989ef48c
Walid Ahmad <walid33@gmail.com>
2014-07-31 01:11:26 -0400

    make a basic user model (including secure passwords)

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe "UserPages" do
-subject { page }
+	subject { page }
 
-  describe "signup page" do
-    before { visit signup_path }
+	  describe "signup page" do
+	    before { visit signup_path }
 
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
+	    it { should have_content('Sign up') }
+	    it { should have_title(full_title('Sign up')) }
+	end
 end
+
